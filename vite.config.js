6b9c11665929353f6fe27e66d28c7252eb479d32
jingwufang <jfan0087@student.monash.edu>
2026-08-05 11:23:20 +0800

    Update the base path in the Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Nonash-library-Jingwu-Fang/',
   plugins: [
     vue(),
     vueDevTools(),
